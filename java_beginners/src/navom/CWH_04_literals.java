ec7ed6a1fe5579fafcdaeb1d61dac5e34120fcb9
navom7 <navo.akp@gmail.com>
2023-02-09 02:08:51 +0530

    java programs

---
+++ java_beginners/src/navom/CWH_04_literals.java
@@ -0,0 +1,8 @@
+package navom;
+
+public class CWH_04_literals {
+    public static void Main(String[] args) {
+        byte age = 126;
+        System.out.println(age);
+    }
+}
---
+++ java_beginners/src/navom/Main.java
@@ -0,0 +1,11 @@
+package navom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int num1 = 7;
+        int num2 = 8;
+        int num3 = 9;
+        System.out.println(num1+num2+num3);
+    }
+}
