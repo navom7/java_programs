d49a457b2b754b0b42b81806adb94211a51ab727
navom7 <navo.akp@gmail.com>
2023-04-04 23:17:25 +0530

    nanhe pandey

--- java_beginners/src/navom/Main.java
+++ java_beginners/src/navom/Main.java
@@ -7,5 +7,7 @@ public class Main {
         int num2 = 8;
         int num3 = 9;
         System.out.println(num1+num2+num3);
+        String name = "Nanhe Kumar Pandey";
+        System.out.println(name);
     }
 }
